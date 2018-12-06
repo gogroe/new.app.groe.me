3efd534b28604b7b1349df44838df9e6301a4486
Jack Schmidt <js@wwhat.de>
2018-12-06 12:11:42 +0100

    New user-modules task, admin

---
+++ src/users/feed/admin/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <p>vita</p>
+    <pre>{{u_task.data}}</pre>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Request from "../../../components/functions/request";
+export default {
+  name: "users_task",
+  data(){
+    return{
+      u_task: {
+        params: {
+          user_id: null
+        },
+        url: 'https://newbackend.groe.me/users/get_user_task',
+        data: {},
+        request: false
+      }
+    }
+  },
+  computed:{
+    ...mapGetters([
+      'user_vitas'
+    ]),
+    u_vita_data(){
+      return this.u_vita.data
+    }
+  },
+  watch:{
+    u_vita_data: function(object) {
+      this.$store.commit('add_user_vitas', object)
+    }
+  },
+  // mounted(){
+  //   this.u_vita.request = true
+  // }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/users/feed/task/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <p>vita</p>
+    <pre>{{u_vita.data}}</pre>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Request from "../../../components/functions/request";
+export default {
+  name: "users_vita",
+  data(){
+    return{
+      u_vita: {
+        params: {
+
+        },
+        url: 'https://newbackend.groe.me/users/get_user_vita',
+        data: {},
+        request: false
+      }
+    }
+  },
+  computed:{
+    ...mapGetters([
+      'user_vitas'
+    ]),
+    u_vita_data(){
+      return this.u_vita.data
+    }
+  },
+  watch:{
+    u_vita_data: function(object) {
+      this.$store.commit('add_user_vitas', object)
+    }
+  },
+  mounted(){
+    this.u_vita.request = true
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
