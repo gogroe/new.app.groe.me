a28ffcf4140e8089f07dbd68ac8e83d716413712
Jack Schmidt <js@wwhat.de>
2018-12-07 12:53:04 +0100

    errors

---
+++ src/components/errors/index.vue
@@ -0,0 +1,63 @@
+<template>
+    <div v-if="error.length > 0">
+      <p :class="error_class">{{error}}</p>
+    </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  export default {
+    name: "errors",
+    props:{
+      request_data:{
+        required: true
+      },
+      input_name:{
+        required: true
+      },
+      error_class:{
+        required: false
+      }
+    },
+    data(){
+      return{
+        error: ''
+      }
+    },
+    computed:{
+      ...mapGetters([
+        'list_states'
+      ])
+    },
+    watch:{
+      request_data: function () {
+        this.check_errors()
+      }
+    },
+    mounted(){
+      this.check_errors()
+    },
+    methods:{
+      check_errors(){
+        if('errors' in this.request_data){
+          if((typeof this.request_data.errors) === 'Object' ||
+             (typeof this.request_data.errors) === 'Array'){
+
+            for(let key in this.request_data.errors){
+              if(this.input_name === key){
+                if('error' in this.request_data.errors[key]){
+                  this.error = this.list_states[this.request_data.errors[key].error]
+                }
+              }
+            }
+            
+          }
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
